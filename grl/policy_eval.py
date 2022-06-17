9d4a619dd264dda78bf62a271106554daa03e9bc
Cam Allen <csal@brown.edu>
2022-06-17 12:44:06 -0400

    Make yapf happy with long comments

--- grl/policy_eval.py
+++ grl/policy_eval.py
@@ -2,12 +2,14 @@ import logging
 
 import jax.numpy as np
 from jax.config import config
+
 config.update("jax_enable_x64", True)
 config.update('jax_platform_name', 'cpu')
 
 from .mdp import MDP
 
 class PolicyEval:
+
     def __init__(self, amdp, verbose=True):
         """
         :param amdp:    AMDP
@@ -47,8 +49,10 @@ class PolicyEval:
         """
         # Each index of these lists corresponds to one linear equation
         # b = A*V_pi(s)
-        A = [] # Each index will contain a list of |S| coefficients for that equation (list of lists of floats)
-        b = [] # Each index will contain the sum of the constants for that equation   (list of floats)
+        # Each index of A will contain a list of |S| coefficients for that equation (list of lists of floats)
+        # Each index of b will contain the sum of the constants for that equation   (list of floats)
+        A = []
+        b = []
         for s in range(mdp.n_states):
             a_t = np.zeros(mdp.n_states)
             # a_t[s] = -1 # subtract V_pi(s) to right side
@@ -57,8 +61,8 @@ class PolicyEval:
             T_pi = np.tensordot(self.pi_ground[s], mdp.T, axes=1)
             R_pi = np.tensordot(self.pi_ground[s], mdp.R * mdp.T, axes=1)
             for next_s in range(mdp.n_states):
-                t = T_pi[s,next_s]
-                r = R_pi[s,next_s]
+                t = T_pi[s, next_s]
+                r = R_pi[s, next_s]
                 # a_t[next_s] += t * mdp.gamma # add V_pi(s') to right side
                 a_t = a_t.at[next_s].set(a_t[next_s] + t * mdp.gamma)
                 b_t -= r # subtract constants to left side
@@ -66,7 +70,7 @@ class PolicyEval:
             A.append(a_t)
             b.append(b_t)
 
-        v_vals =  np.linalg.solve(A, b)
+        v_vals = np.linalg.solve(A, b)
 
         # Q vals
         q_vals = np.zeros((mdp.n_states, mdp.n_actions))
@@ -74,7 +78,8 @@ class PolicyEval:
             for a in range(mdp.n_actions):
                 for sp in range(mdp.n_states):
                     # q_vals[s,a] += mdp.R[a,s,sp] + mdp.gamma * mdp.T[a,s,sp] * v_vals[sp]
-                    q_vals = q_vals.at[s,a].set(q_vals[s,a] + mdp.T[a,s,sp] * (mdp.R[a,s,sp] + mdp.gamma * v_vals[sp]))
+                    q_vals = q_vals.at[s, a].set(q_vals[s, a] + mdp.T[a, s, sp] *
+                                                 (mdp.R[a, s, sp] + mdp.gamma * v_vals[sp]))
 
         return {'v': v_vals, 'q': q_vals}
 
@@ -93,7 +98,7 @@ class PolicyEval:
             a_t = a_t.at[s].set(-1)
             for prev_s in range(self.amdp.n_states):
                 T_pi = np.tensordot(self.pi_ground[prev_s], self.amdp.T, axes=1)
-                t = T_pi[prev_s,s]
+                t = T_pi[prev_s, s]
                 if not no_gamma:
                     t *= self.amdp.gamma
                 # a_t[prev_s] += t
@@ -112,7 +117,7 @@ class PolicyEval:
         q_vals = np.zeros((self.amdp.n_obs, self.amdp.n_actions))
         for ob in range(self.amdp.n_obs):
             # V vals
-            col = self.amdp.phi[:,ob].copy().astype('float')
+            col = self.amdp.phi[:, ob].copy().astype('float')
             col *= occupancy
             col /= col.sum()
             v = mdp_vals * col
@@ -125,9 +130,8 @@ class PolicyEval:
                     for sp in range(self.amdp.n_states):
                         q_vals = q_vals.at[
                             ob, a].set(q_vals[ob, a] + col[s] *
-                                      (self.amdp.T[a, s, sp] *
-                                       (self.amdp.R[a, s, sp] +
-                                        self.amdp.gamma * mdp_vals[sp])))
+                                       (self.amdp.T[a, s, sp] *
+                                        (self.amdp.R[a, s, sp] + self.amdp.gamma * mdp_vals[sp])))
 
         return {'v': v_vals, 'q': q_vals}
 
@@ -144,11 +148,11 @@ class PolicyEval:
             p_π_of_o_given_s = self.amdp.phi[:, curr_ob].copy().astype('float')
             # want p_π(s|o) ∝ p_π(o|s)p(s) = p_π_of_o_given_s * occupancy
             w = occupancy * p_π_of_o_given_s # Prob of being in each state * prob of it emitting curr obs i
-            p_π_of_s_given_o = (w / w.sum())[:,None]
+            p_π_of_s_given_o = (w / w.sum())[:, None]
 
             for next_ob in range(self.amdp.n_obs):
                 # Q: what action should this be? [self.pi[i]]
-                p_π_of_op_given_sp = self.amdp.phi[:,next_ob].copy().astype('float')
+                p_π_of_op_given_sp = self.amdp.phi[:, next_ob].copy().astype('float')
 
                 # T
                 T_contributions = (self.amdp.T * p_π_of_s_given_o * p_π_of_op_given_sp)
@@ -158,8 +162,9 @@ class PolicyEval:
 
                 # R
                 R_contributions = self.amdp.R * T_contributions
-                denom = T_obs_obs[:,curr_ob,next_ob][:, None, None]
-                denom = np.where(denom == 0, 1, denom) # Avoid divide by zero (there may be a better way)
+                denom = T_obs_obs[:, curr_ob, next_ob][:, None, None]
+                denom = np.where(denom == 0, 1,
+                                 denom) # Avoid divide by zero (there may be a better way)
                 R_contributions /= denom
 
                 # R_obs_obs[:,curr_ob,next_ob] = R_contributions.sum(2).sum(1)
