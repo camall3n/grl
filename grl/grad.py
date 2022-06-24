d3892b2bf52d40d72f4b71057ebe872a48aabfda
dsctt <dascott321@gmail.com>
2022-06-24 16:14:17 -0400

    Add max loss; transpose q (now AxS); add q to grad and heatmap

--- grl/grad.py
+++ grl/grad.py
@@ -5,29 +5,49 @@ from .utils import pformat_vals
 import numpy as np
 from jax import grad
 
-def do_grad(policy_eval, pi_abs, lr=1):
+def do_grad(policy_eval, pi_abs, value_type='v', discrep_type='l2', lr=1):
     """
-    :param policy_eval: PolicyEval object
-    :param pi_abs:      policy over abstract state space
-    :param lr:          learning rate
+    :param policy_eval:   PolicyEval object
+    :param pi_abs:        policy over abstract state space
+    :param lr:            learning rate
+    :param value_type:  'v' or 'q'
+    :param discrep_type:   'l2' or 'max'
+        - 'l2' uses MSE over all obs(/actions)
+        - 'max' uses the highest individual absolute difference across obs(/actions) 
+        - (see policy_eval.py)
+        - Currently has to be adjusted above directly
     """
+
+    if value_type == 'v':
+        if discrep_type == 'l2':
+            grad_fn = grad(policy_eval.mse_loss_v)
+        elif discrep_type == 'max':
+            grad_fn = grad(policy_eval.max_loss_v)
+    elif value_type == 'q':
+        if discrep_type == 'l2':
+            grad_fn = grad(policy_eval.mse_loss_q)
+        elif discrep_type == 'max':
+            grad_fn = grad(policy_eval.max_loss_q)
 
     policy_eval.verbose = False
     old_pi = pi_abs
     i = 0
     done_count = 0
+
     while done_count < 5:
         i += 1
         if i % 10 == 0:
             print('Gradient iteration', i)
 
-        pi_grad = grad(policy_eval.mse_loss)(pi_abs)
+        pi_grad = grad_fn(pi_abs)
         old_pi = pi_abs
         pi_abs -= lr * pi_grad
 
         # Normalize
         pi_abs = pi_abs.clip(0, 1)
-        pi_abs /= pi_abs.sum(axis=1, keepdims=True)
+        denom = pi_abs.sum(axis=1, keepdims=True)
+        denom = np.where(denom == 0, 1, denom) # Avoid divide by zero (there may be a better way)
+        pi_abs /= denom
 
         if np.allclose(old_pi, pi_abs):
             done_count += 1
