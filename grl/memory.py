43a1e630dbd0b6d8d079362e71f0798fe56a05e7
dsctt <dascott321@gmail.com>
2022-07-18 11:10:06 -0400

    Repeat rewards

--- grl/memory.py
+++ grl/memory.py
@@ -18,23 +18,21 @@ def memory_cross_product(amdp, T_mem):
     R_x = np.zeros((R.shape[0], n_states_x, n_states_x))
     phi_x = np.zeros((n_states_x, phi.shape[-1] * T_mem.shape[-1]))
 
+    # Rewards only depend on MDP (not memory function)
+    R_x = R.repeat(n_states_m, axis=1).repeat(n_states_m, axis=2)
+
     for s in range(amdp.n_states):
-        T_phi_mem = np.tensordot(phi[s][:-1], T_mem, 1) # T_mem does not include terminal obs
+        T_phi_mem = np.tensordot(phi[s][:-1], T_mem, axes=1) # T_mem does not include terminal obs
         rows = np.multiply.outer(T[:, s],
                                  T_phi_mem).swapaxes(1, 2).reshape(T.shape[0], n_states_m,
                                                                    n_states_x)
         T_x[:, (s * n_states_m):(s * n_states_m + n_states_m)] = rows
 
-        # Rewards only depend on MDP (not memory function)
-        R_x[:, (s * n_states_m):(s * n_states_m + n_states_m)] = np.repeat(R[:, s],
-                                                                           n_states_m,
-                                                                           axis=1)[:, None]
-
         for s_mem in range(n_states_m):
             phi_x[s * n_states_m + s_mem, s_mem::n_states_m] = phi[s]
 
-    mdp = MDP(T_x, R_x, amdp.gamma)
+    mdp_x = MDP(T_x, R_x, amdp.gamma)
     # Assuming memory starts with all 0s
     p0_x = np.zeros(T_x.shape[-1])
     p0_x[::n_states_m] = amdp.p0
-    return AbstractMDP(mdp, phi_x, p0=p0_x)
+    return AbstractMDP(mdp_x, phi_x, p0=p0_x)
