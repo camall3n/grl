dfb9ce4fe1b8c5d2cbd4a103f0cb79db0061fa15
dsctt <dascott321@gmail.com>
2022-07-18 16:30:31 -0400

    Refactor T_x calc

--- grl/memory.py
+++ grl/memory.py
@@ -16,19 +16,22 @@ def memory_cross_product(amdp, T_mem):
     n_states_x = T_mem.shape[-1] * T.shape[-1] # cross (x) product MDP
     T_x = np.zeros((T.shape[0], n_states_x, n_states_x))
     R_x = np.zeros((R.shape[0], n_states_x, n_states_x))
-    phi_x = np.zeros((n_states_x, phi.shape[-1] * T_mem.shape[-1]))
+    phi_x = np.zeros((n_states_x, phi.shape[-1] * n_states_m))
 
     # Rewards only depend on MDP (not memory function)
     R_x = R.repeat(n_states_m, axis=1).repeat(n_states_m, axis=2)
 
+    T_phi_mem = np.tensordot(phi, T_mem, axes=1)
+    T_x_big = np.multiply.outer(T, T_phi_mem).swapaxes(2, 3).swapaxes(3, 4).reshape(2, 4, 8, 8)
+
     for s in range(amdp.n_states):
-        T_phi_mem = np.tensordot(phi[s][:-1], T_mem, axes=1) # T_mem does not include terminal obs
-        rows = np.multiply.outer(T[:, s],
-                                 T_phi_mem).swapaxes(1, 2).reshape(T.shape[0], n_states_m,
-                                                                   n_states_x)
-        T_x[:, (s * n_states_m):(s * n_states_m + n_states_m)] = rows
+        start = s * n_states_m
+        end = s * n_states_m + n_states_m
+        T_x[:, start:end] = T_x_big[:, s, start:end]
 
         for s_mem in range(n_states_m):
+            # The new obs_x are the original obs times memory states
+            # E.g. obs={r,b} and mem={0,1} -> obs_x={r0,r1,b0,b1}
             phi_x[s * n_states_m + s_mem, s_mem::n_states_m] = phi[s]
 
     mdp_x = MDP(T_x, R_x, amdp.gamma)
