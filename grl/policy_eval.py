c34d986580fddfe8c201ef9a1998e6e29bab85d4
Ruo Yu Tao <ruoyutao@gmail.com>
2022-10-12 15:21:37 -0400

    about to start writing tests

--- grl/policy_eval.py
+++ grl/policy_eval.py
@@ -1,5 +1,3 @@
-import logging
-
 import numpy as np
 import jax.numpy as jnp
 from jax import jit, value_and_grad, nn
@@ -8,15 +6,120 @@ from functools import partial
 from .mdp import MDP
 from .memory import functional_memory_cross_product, memory_cross_product
 
-def normalize_rows(mat: jnp.ndarray):
-    # Normalize (assuming params are probability distribution)
-    mat = mat.clip(0, 1)
-    denom = mat.sum(axis=-1, keepdims=True)
-    denom_no_zero = denom + (denom == 0).astype(denom.dtype)
-    mat /= denom_no_zero
+@partial(jit, static_argnames=['gamma'])
+def functional_get_occupancy(pi_ground: jnp.ndarray, T: jnp.ndarray, p0: jnp.ndarray,
+                             gamma: float):
+    Pi_pi = pi_ground.transpose()[..., None]
+    T_pi = (Pi_pi * T).sum(axis=0)  # T^π(s'|s)
 
-    # mat = nn.softmax(mat, axis=-1)
-    return mat
+    # A*C_pi(s) = b
+    # A = (I - \gamma (T^π)^T)
+    # b = P_0
+    A = jnp.eye(T.shape[-1]) - gamma * T_pi.transpose()
+    b = p0
+    return jnp.linalg.solve(A, b)
+
+@jit
+def get_p_s_given_o(phi: jnp.ndarray, occupancy: jnp.ndarray):
+    repeat_occupancy = jnp.repeat(occupancy[..., None], phi.shape[-1], -1)
+
+    # Q vals
+    p_of_o_given_s = phi.astype('float')
+    w = repeat_occupancy * p_of_o_given_s
+
+    p_pi_of_s_given_o = w / (w.sum(axis=0) + 1e-10)
+    return p_pi_of_s_given_o
+
+@partial(jit, static_argnames=['gamma'])
+def functional_solve_mdp(pi: jnp.ndarray, T: jnp.ndarray, R: jnp.ndarray, gamma: float):
+    """
+    Solves for V using linear equations.
+    For all s, V_pi(s) = sum_s' sum_a[T(s'|s,a) * pi(a|s) * (R(s,a,s') + gamma * V_pi(s'))]
+    """
+    Pi_pi = pi.transpose()[..., None]
+    T_pi = (Pi_pi * T).sum(axis=0)  # T^π(s'|s)
+    R_pi = (Pi_pi * T * R).sum(axis=0).sum(axis=-1)  # R^π(s)
+
+    # A*V_pi(s) = b
+    # A = (I - \gamma (T^π))
+    # b = R^π
+    A = (jnp.eye(T.shape[-1]) - gamma * T_pi)
+    b = R_pi
+    v_vals = jnp.linalg.solve(A, b)
+
+    R_sa = (T * R).sum(axis=-1)  # R(s,a)
+    q_vals = (R_sa + (gamma * T @ v_vals))
+
+    return v_vals, q_vals
+
+@jit
+def functional_solve_amdp(mdp_q_vals: jnp.ndarray, p_pi_of_s_given_o: jnp.ndarray,
+                          pi_abs: jnp.ndarray):
+    # Q vals
+    amdp_q_vals = mdp_q_vals @ p_pi_of_s_given_o
+
+    # V vals
+    amdp_v_vals = (amdp_q_vals * pi_abs.T).sum(0)
+
+    return {'v': amdp_v_vals, 'q': amdp_q_vals}
+
+@jit
+def functional_create_td_model(p_pi_of_s_given_o: jnp.ndarray, phi: jnp.ndarray,
+                               T: jnp.ndarray, R: jnp.ndarray):
+    # creates an (n_obs * n_obs) x 2 array of all possible observation to observation pairs.
+    # we flip here so that we have curr_obs, next_obs (order matters).
+    obs_idx_product = jnp.flip(
+        jnp.dstack(jnp.meshgrid(jnp.arange(phi.shape[-1]), jnp.arange(phi.shape[-1]))).reshape(-1, 2), -1)
+
+    # this gives us (n_obs * n_obs) x states x 1 and (n_obs * n_obs) x 1 x states
+    curr_s_given_o = p_pi_of_s_given_o[:, obs_idx_product[:, 0]].T[..., None]
+    next_o_given_s = jnp.expand_dims(phi[:, obs_idx_product[:, 1]].T, 1)
+
+    # outer product here
+    o_to_next_o = jnp.expand_dims(curr_s_given_o * next_o_given_s, 1)
+
+    # This is p(o, s, a, s', o')
+    # the probability that o goes to o', via each path (s, a) -> s'.
+    # Shape is (n_obs * n_obs) x |A| x |S| x |S|
+    T_contributions = T * o_to_next_o
+
+    # |A| x (n_obs * n_obs)
+    T_obs_obs_flat = T_contributions.sum(-1).sum(-1).T
+
+    # |A| x n_obs x n_obs
+    T_obs_obs = T_obs_obs_flat.reshape(T.shape[0], phi.shape[-1], phi.shape[-1])
+
+    # You want everything to sum to one
+    denom = T_obs_obs_flat.T[..., None, None]
+    denom_no_zero = denom + (denom == 0).astype(denom.dtype)
+
+    R_contributions = (R * T_contributions) / denom_no_zero
+    R_obs_obs_flat = R_contributions.sum(-1).sum(-1).T
+    R_obs_obs = R_obs_obs_flat.reshape(R.shape[0], phi.shape[-1], phi.shape[-1])
+
+    return T_obs_obs, R_obs_obs
+
+@partial(jit, static_argnames=['gamma'])
+def analytical_pe(pi_abs: jnp.ndarray, phi: jnp.ndarray, T: jnp.ndarray,
+                  R: jnp.ndarray, p0: jnp.ndarray, gamma: float):
+    # observation policy, but expanded over states
+    pi_ground = phi @ pi_abs
+
+    # MC*
+    state_v, state_q = functional_solve_mdp(pi_ground, T, R, gamma)
+    state_vals = {'v': state_v, 'q': state_q}
+
+    occupancy = functional_get_occupancy(pi_ground, T, p0, gamma)
+
+    p_pi_of_s_given_o = get_p_s_given_o(phi, occupancy)
+    mc_vals = functional_solve_amdp(state_q, p_pi_of_s_given_o, pi_abs)
+
+    # TD
+    T_obs_obs, R_obs_obs = functional_create_td_model(p_pi_of_s_given_o, phi, T, R)
+    td_v_vals, td_q_vals = functional_solve_mdp(pi_abs, T_obs_obs, R_obs_obs, gamma)
+    td_vals = {'v': td_v_vals, 'q': td_q_vals}
+
+    return state_vals, mc_vals, td_vals
 
 class PolicyEval:
     def __init__(self, amdp, verbose=True, discrep_type: str = 'l2'):
@@ -37,72 +140,14 @@ class PolicyEval:
         """
         :param pi_abs: policy to evaluate, defined over abstract state space
         """
-        return self._functional_run(pi_abs, self.amdp.phi, self.amdp.T, self.amdp.R, self.amdp.p0,
-                                    self.amdp.gamma, self.amdp.n_states, self.amdp.n_obs)
+        return analytical_pe(pi_abs, self.amdp.phi, self.amdp.T, self.amdp.R, self.amdp.p0,
+                             self.amdp.gamma)
 
-    @partial(jit, static_argnames=['self', 'gamma', 'n_states', 'n_obs'])
-    def _functional_run(self, pi_abs: jnp.ndarray, phi: jnp.ndarray, T: jnp.ndarray,
-                        R: jnp.ndarray, p0: jnp.ndarray, gamma: float, n_states: int, n_obs: int):
-        pi_ground = phi @ pi_abs
-
-        # MC*
-        state_v, state_q = self._functional_solve_mdp(pi_ground, T, R, gamma, n_states)
-        state_vals = {'v': state_v, 'q': state_q}
-
-        occupancy = self._functional_get_occupancy(pi_ground, T, p0, n_states, gamma)
-
-        p_pi_of_s_given_o = self._get_p_s_given_o(n_obs, phi, occupancy)
-        mc_vals = self._functional_solve_amdp(state_q, p_pi_of_s_given_o, pi_abs)
-
-        # TD
-        T_obs_obs, R_obs_obs = self._functional_create_td_model(p_pi_of_s_given_o, phi, T, R,
-                                                                n_obs)
-        td_v_vals, td_q_vals = self._functional_solve_mdp(pi_abs, T_obs_obs, R_obs_obs, gamma,
-                                                          T_obs_obs.shape[-1])
-        td_vals = {'v': td_v_vals, 'q': td_q_vals}
-
-        return state_vals, mc_vals, td_vals
 
     def _solve_mdp(self, mdp, pi):
-        v, q = self._functional_solve_mdp(pi, mdp.T, mdp.R, mdp.gamma, mdp.n_states)
+        v, q = functional_solve_mdp(pi, mdp.T, mdp.R, mdp.gamma)
 
         return {'v': v, 'q': q}
-
-    @staticmethod
-    @partial(jit, static_argnums=[0])
-    def _get_p_s_given_o(n_obs: int, phi: jnp.ndarray, occupancy: jnp.ndarray):
-        repeat_occupancy = jnp.repeat(occupancy[..., None], n_obs, -1)
-
-        # Q vals
-        p_of_o_given_s = phi.astype('float')
-        w = repeat_occupancy * p_of_o_given_s
-
-        p_pi_of_s_given_o = w / (w.sum(axis=0) + 1e-10)
-        return p_pi_of_s_given_o
-
-    @staticmethod
-    @partial(jit, static_argnames=['gamma', 'n_states'])
-    def _functional_solve_mdp(pi: jnp.ndarray, T: jnp.ndarray, R: jnp.ndarray, gamma: float,
-                              n_states: int):
-        """
-        Solves for V using linear equations.
-        For all s, V_pi(s) = sum_s' sum_a[T(s'|s,a) * pi(a|s) * (R(s,a,s') + gamma * V_pi(s'))]
-        """
-        Pi_pi = pi.transpose()[..., None]
-        T_pi = (Pi_pi * T).sum(axis=0) # T^π(s'|s)
-        R_pi = (Pi_pi * T * R).sum(axis=0).sum(axis=-1) # R^π(s)
-
-        # A*V_pi(s) = b
-        # A = (I - \gamma (T^π))
-        # b = R^π
-        A = (jnp.eye(n_states) - gamma * T_pi)
-        b = R_pi
-        v_vals = jnp.linalg.solve(A, b)
-
-        R_sa = (T * R).sum(axis=-1) # R(s,a)
-        q_vals = (R_sa + (gamma * T @ v_vals))
-
-        return v_vals, q_vals
 
     def get_occupancy(self, pi: jnp.ndarray):
         """
@@ -111,84 +156,20 @@ class PolicyEval:
           where s^ is the prev state
         """
         pi_ground = self.amdp.phi @ pi
-        return self._functional_get_occupancy(pi_ground, self.amdp.T, self.amdp.p0,
-                                              self.amdp.n_states, self.amdp.gamma)
-
-    @staticmethod
-    @partial(jit, static_argnames=['n_states', 'gamma'])
-    def _functional_get_occupancy(pi_ground: jnp.ndarray, T: jnp.ndarray, p0: jnp.ndarray,
-                                  n_states: int, gamma: float):
-        Pi_pi = pi_ground.transpose()[..., None]
-        T_pi = (Pi_pi * T).sum(axis=0) # T^π(s'|s)
-
-        # A*C_pi(s) = b
-        # A = (I - \gamma (T^π)^T)
-        # b = P_0
-        A = jnp.eye(n_states) - gamma * T_pi.transpose()
-        b = p0
-        return jnp.linalg.solve(A, b)
+        return functional_get_occupancy(pi_ground, self.amdp.T, self.amdp.p0,
+                                        self.amdp.n_states, self.amdp.gamma)
 
     def _solve_amdp(self, mdp_q_vals: jnp.ndarray, p_pi_of_s_given_o: jnp.ndarray,
                     pi: jnp.ndarray):
         """
         Weights the value contribution of each state to each observation for the amdp
         """
-        return self._functional_solve_amdp(mdp_q_vals, p_pi_of_s_given_o, pi)
-
-    @partial(jit, static_argnums=0)
-    def _functional_solve_amdp(self, mdp_q_vals: jnp.ndarray, p_pi_of_s_given_o: jnp.ndarray,
-                               pi_abs: jnp.ndarray):
-
-        # Q vals
-        amdp_q_vals = mdp_q_vals @ p_pi_of_s_given_o
-
-        # V vals
-        amdp_v_vals = (amdp_q_vals * pi_abs.T).sum(0)
-
-        return {'v': amdp_v_vals, 'q': amdp_q_vals}
+        return functional_solve_amdp(mdp_q_vals, p_pi_of_s_given_o, pi)
 
     def _create_td_model(self, p_pi_of_s_given_o: jnp.ndarray):
-        T_obs_obs, R_obs_obs = self._functional_create_td_model(p_pi_of_s_given_o, self.amdp.phi,
-                                                                self.amdp.T, self.amdp.R,
-                                                                self.amdp.n_obs)
+        T_obs_obs, R_obs_obs = functional_create_td_model(p_pi_of_s_given_o, self.amdp.phi,
+                                                          self.amdp.T, self.amdp.R)
         return MDP(T_obs_obs, R_obs_obs, self.amdp.p0, self.amdp.gamma)
-
-    @staticmethod
-    @partial(jit, static_argnames=['n_obs'])
-    def _functional_create_td_model(p_pi_of_s_given_o: jnp.ndarray, phi: jnp.ndarray,
-                                    T: jnp.ndarray, R: jnp.ndarray, n_obs: int):
-        # creates an (n_obs * n_obs) x 2 array of all possible observation to observation pairs.
-        # we flip here so that we have curr_obs, next_obs (order matters).
-        obs_idx_product = jnp.flip(
-            jnp.dstack(jnp.meshgrid(jnp.arange(n_obs), jnp.arange(n_obs))).reshape(-1, 2), -1)
-
-        # this gives us (n_obs * n_obs) x states x 1 and (n_obs * n_obs) x 1 x states
-        curr_s_given_o = p_pi_of_s_given_o[:, obs_idx_product[:, 0]].T[..., None]
-        next_o_given_s = jnp.expand_dims(phi[:, obs_idx_product[:, 1]].T, 1)
-
-        # outer product here
-        o_to_next_o = jnp.expand_dims(curr_s_given_o * next_o_given_s, 1)
-
-        # This is p(o, s, a, s', o')
-        # the probability that o goes to o', via each path (s, a) -> s'.
-        # Shape is (n_obs * n_obs) x |A| x |S| x |S|
-        T_contributions = T * o_to_next_o
-
-        # |A| x (n_obs * n_obs)
-        T_obs_obs_flat = T_contributions.sum(-1).sum(-1).T
-
-        # |A| x n_obs x n_obs
-        T_obs_obs = T_obs_obs_flat.reshape(T.shape[0], n_obs, n_obs)
-
-        # You want everything to sum to one
-        denom = T_obs_obs_flat.T[..., None, None]
-        denom_no_zero = denom + (denom == 0).astype(denom.dtype)
-
-        R_contributions = (R * T_contributions) / denom_no_zero
-        R_obs_obs_flat = R_contributions.sum(-1).sum(-1).T
-        R_obs_obs = R_obs_obs_flat.reshape(R.shape[0], n_obs, n_obs)
-
-        return T_obs_obs, R_obs_obs
 
     ##########
     # Helpers for gradient/heatmap stuff
@@ -205,8 +186,7 @@ class PolicyEval:
     @partial(jit, static_argnames=['self', 'value_type', 'gamma'])
     def functional_mse_loss(self, pi: jnp.ndarray, value_type: str, phi: jnp.ndarray,
                             T: jnp.ndarray, R: jnp.ndarray, p0: jnp.ndarray, gamma: float):
-        _, mc_vals, td_vals = self._functional_run(pi, phi, T, R, p0, gamma, T.shape[-1],
-                                                   phi.shape[-1])
+        _, mc_vals, td_vals = analytical_pe(pi, phi, T, R, p0, gamma)
         diff = mc_vals[value_type] - td_vals[value_type]
         return (diff**2).mean()
 
@@ -220,8 +200,7 @@ class PolicyEval:
     @partial(jit, static_argnames=['self', 'value_type', 'gamma'])
     def functional_max_loss(self, pi: jnp.ndarray, value_type: str, phi: jnp.ndarray,
                             T: jnp.ndarray, R: jnp.ndarray, p0: jnp.ndarray, gamma: float):
-        _, mc_vals, td_vals = self._functional_run(pi, phi, T, R, p0, gamma, T.shape[-1],
-                                                   phi.shape[-1])
+        _, mc_vals, td_vals = analytical_pe(pi, phi, T, R, p0, gamma)
         return jnp.abs(mc_vals[value_type] - td_vals[value_type]).max()
 
     def memory_loss(self, mem_params, value_type, **kwargs):
@@ -237,12 +216,10 @@ class PolicyEval:
     def functional_policy_update(self, params: jnp.ndarray, value_type: str, gamma: float,
                                  lr: float, T: jnp.ndarray, R: jnp.ndarray, phi: jnp.ndarray,
                                  p0: jnp.ndarray):
+        pi = nn.softmax(params, axis=-1)
         loss, params_grad = value_and_grad(self.functional_loss_fn,
-                                           argnums=0)(params, value_type, phi, T, R, p0, gamma)
+                                           argnums=0)(pi, value_type, phi, T, R, p0, gamma)
         params -= lr * params_grad
-
-        # Normalize (assuming params are probability distribution)
-        params = normalize_rows(params)
         return loss, params
 
     def memory_update(self, mem_params: jnp.ndarray, value_type: str, lr: float, pi: jnp.ndarray):
