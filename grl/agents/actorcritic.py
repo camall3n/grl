7e8f8df5669d96b7f1dd18040101c3f13aa9b8a6
Cam Allen <csal@brown.edu>
2023-01-10 04:49:13 -0500

    Pull out the policy/memory caching into functions

--- grl/agents/actorcritic.py
+++ grl/agents/actorcritic.py
@@ -65,6 +65,14 @@ class ActorCritic:
         self.q_td.update(**augmented_experience)
         self.q_mc.update(**augmented_experience)
 
+    def set_policy(self, params):
+        self.policy_params = params
+        self.cached_policy = softmax(self.policy_params, axis=-1)
+
+    def set_memory(self, params):
+        self.memory_params = params
+        self.cached_memory_fn = softmax(self.memory_params, axis=-1)
+
     def optimize_memory(self):
         study = optuna.create_study(direction='minimize', sampler=optuna.samplers.CmaEsSampler)
         n_params = np.prod(self.memory_params.shape)
@@ -75,8 +83,7 @@ class ActorCritic:
         def objective(trial: optuna.Trial):
             params = np.asarray([suggest_param(trial, str(i))
                                  for i in range(n_params)]).reshape(self.memory_params.shape)
-            self.memory_params = params
-            self.cached_memory_fn = softmax(self.memory_params, axis=-1)
+            self.set_memory(params)
             return self.evaluate_memory()
 
         study.optimize(objective, n_trials=100)
@@ -113,8 +120,7 @@ class ActorCritic:
         # augment last dim with input mem states
         self.n_obs *= n_mem_states
         pi_augmented = np.expand_dims(self.policy_params, 1).repeat(n_mem_states, 1) # O x M x A
-        self.policy_params = pi_augmented.reshape(self.n_obs, self.n_actions) # OM x A
-        self.cached_policy = softmax(self.policy_params, axis=-1)
+        self.set_policy(pi_augmented.reshape(self.n_obs, self.n_actions)) # OM x A
 
     def add_memory(self, n_mem_entries=1):
         mem_increase_multiplier = (self.n_mem_values**n_mem_entries)
