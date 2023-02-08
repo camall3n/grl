cfe8d08f2d3a59bb2cc035a33cff2a14394b5a58
Cam Allen <csal@brown.edu>
2023-02-08 18:43:14 -0500

    Refactor tmaze_junction_sweep_viz for both planning AND learning

--- scripts/plotting/tmaze_junction_sweep_viz.py
+++ scripts/plotting/tmaze_junction_sweep_viz.py
@@ -1,27 +1,18 @@
-import numpy as np
-import jax.numpy as jnp
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from jax.nn import softmax
+import glob
 from pathlib import Path
-from collections import namedtuple
+import os
 
-np.set_printoptions(precision=4)
-plt.rcParams['axes.facecolor'] = 'white'
-plt.rcParams.update({'font.size': 14})
+import jax.numpy as jnp
+from jax.nn import softmax
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 from grl.utils import load_info
 from definitions import ROOT_DIR
 
-
-# %%
-results_dir = Path(ROOT_DIR, 'results', 'analytical', 'tmaze_sweep_junction_pi_old')
-
-split_by = ['spec', 'algo', 'tmaze_corridor_length', 'tmaze_junction_up_pi']
-Args = namedtuple('args', split_by)
-
-# %%
-
+#%%
 def test_mem_matrix(mem_params: jnp.ndarray):
     """
     Tests the memory matrix for t-maze.
@@ -57,169 +48,137 @@ def test_mem_matrix(mem_params: jnp.ndarray):
     is_toggle, is_hold = test_corridor_hold_or_toggle(right_corridor)
     return diff_start_bits_set, is_toggle, is_hold
 
-# %%
-all_results = {}
+#%%
+def load_sampled_results(pathname: str):
+    all_results = []
+    results_dirs = glob.glob(pathname)
+    for results_dir in results_dirs:
+        info = load_info(results_dir + '/info.npy')
+        info['policy_up_prob']
+        info.keys()
+        final_mem_params = info['final_params']
+        diff_start_bits_set, is_toggle, is_hold = test_mem_matrix(final_mem_params)
+        is_optimal = diff_start_bits_set and (is_toggle or is_hold)
+        result = {
+            'policy_up_prob': info['policy_up_prob'],
+            'trial_id': os.path.basename(results_dir).split('__')[0].split('_')[-1],
+            'initial_discrep': info['initial_discrep'],
+            'final_discrep': info['final_discrep'],
+            'is_optimal': is_optimal
+        }
+        all_results.append(result)
+    data = pd.DataFrame(all_results)
+    return data
 
-for results_path in results_dir.iterdir():
-    if results_path.suffix != '.npy':
-        continue
-    info = load_info(results_path)
-    grad_info = info['grad_info']
-    args = info['args']
-    final_mem_params = grad_info['final_params']
+#%%
+def load_analytical_results(pathname: str):
+    results_dir = Path(ROOT_DIR, pathname)
 
-    softmax(final_mem_params, axis=-1)
-    diff_start_bits_set, is_toggle, is_hold = test_mem_matrix(final_mem_params)
-    initial_q_discrep, initial_v_discrep = info['initial_discrep']['q'], info['initial_discrep'][
-        'v']
+    all_results = []
+    for results_path in results_dir.iterdir():
+        if results_path.suffix != '.npy':
+            continue
+        info = load_info(results_path)
+        grad_info = info['grad_info']
+        args = info['args']
+        final_mem_params = grad_info['final_params']
 
-    final_mc_vals, final_td_vals = grad_info['final_vals']['mc'], grad_info['final_vals']['td']
-    final_v_discrep = (final_mc_vals['v'] - final_td_vals['v'])**2
-    final_q_discrep = (final_mc_vals['q'] - final_td_vals['q'])**2
+        diff_start_bits_set, is_toggle, is_hold = test_mem_matrix(final_mem_params)
+        initial_q_discrep, initial_v_discrep = info['initial_discrep']['q'], info[
+            'initial_discrep']['v']
 
-    single_res = {
-        'final_params': grad_info['final_params'],
-        'diff_start_bits_set': diff_start_bits_set,
-        'is_toggle': is_toggle,
-        'is_hold': is_hold,
-        'is_optimal': diff_start_bits_set and (is_toggle or is_hold),
-        'tmaze_junction_up_pi': args['tmaze_junction_up_pi'],
-        'initial_v_discrep': initial_v_discrep,
-        'initial_q_discrep': initial_q_discrep,
-        'final_v_discrep': final_v_discrep,
-        'final_q_discrep': final_q_discrep,
-    }
+        final_mc_vals, final_td_vals = grad_info['final_vals']['mc'], grad_info['final_vals']['td']
+        final_v_discrep = (final_mc_vals['v'] - final_td_vals['v'])**2
+        final_q_discrep = (final_mc_vals['q'] - final_td_vals['q'])**2
 
-    hparams = Args(*tuple(args[s] for s in split_by))
+        policy_up_prob = args['tmaze_junction_up_pi']
 
-    if hparams not in all_results:
-        all_results[hparams] = {}
+        def aggregate_q_discrep(q_discrep):
+            p = policy_up_prob
+            policy = np.array([
+                [0., 0., 1., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 1., 0.],
+                [p, (1 - p), 0., 0.],
+                [p, (1 - p), 0., 0.],
+                [1., 0., 0., 0.],
+                [1., 0., 0., 0.],
+            ])
+            return (q_discrep.T * policy).sum(-1).mean()
 
-    for k, v in single_res.items():
-        if k not in all_results[hparams]:
-            all_results[hparams][k] = []
-        all_results[hparams][k].append(v)
-    all_results[hparams]['args'] = args
+        result = {
+            'policy_up_prob': policy_up_prob,
+            'trial_id': os.path.basename(results_path).split('_s(')[-1].split(')_')[0],
+            # 'initial_discrep': initial_v_discrep.mean(),
+            # 'final_discrep': final_v_discrep.mean(),
+            'initial_discrep': aggregate_q_discrep(initial_q_discrep),
+            'final_discrep': aggregate_q_discrep(final_q_discrep),
+            'is_optimal': diff_start_bits_set and (is_toggle or is_hold),
+        }
+        all_results.append(result)
+    data = pd.DataFrame(all_results)
+    return data
 
-for hparams, res_dict in all_results.items():
-    for k, v in res_dict.items():
-        if k != 'args':
-            all_results[hparams][k] = np.stack(v)
-# %%
+#%%
+def plot_sweep(data: pd.DataFrame, ax=None, title=None, add_colorbar=False):
+    y_high = max(data['initial_discrep'].max(), data['final_discrep'].max()) * 1.05
+    y_low = 0
+    x_high = max(data['policy_up_prob'])
+    x_low = min(data['policy_up_prob'])
 
-def get_bool_ranges(bools: np.ndarray, x: np.ndarray):
-    true_ranges = []
-    false_ranges = []
+    np.set_printoptions(precision=4)
+    plt.rcParams['axes.facecolor'] = 'white'
+    plt.rcParams.update({'font.size': 14})
 
-    current_start = x[0]
-    current_bool = bools[0]
-    for b, potential_end in zip(is_optimals[1:], x[1:]):
-        if b != current_bool:
-            r = (current_start, potential_end)
-            if current_bool:
-                true_ranges.append(r)
-            else:
-                false_ranges.append(r)
-            current_start = potential_end
-            current_bool = b
-    else:
-        r = (current_start, potential_end)
-        if current_bool:
-            true_ranges.append(r)
-        else:
-            false_ranges.append(r)
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(7, 5))
+    data_seed_avg = data.groupby('policy_up_prob', as_index=False).mean()
+    aximg = ax.imshow(
+        data_seed_avg['is_optimal'].to_numpy()[None, :],
+        vmin=0,
+        vmax=1,
+        extent=(x_low, x_high, y_low, y_high),
+        cmap='RdYlBu',
+        aspect='auto',
+        alpha=0.5,
+    )
+    if add_colorbar:
+        plt.gcf().colorbar(aximg,
+                           label='Optimal memory frequency',
+                           ticks=[0.0, 0.5, 1.0],
+                           format='%0.1f')
+    sns.lineplot(
+        data=data,
+        x='policy_up_prob',
+        y='initial_discrep',
+        color='blue',
+        ax=ax,
+        label='Initial',
+    )
+    sns.lineplot(
+        data=data,
+        x='policy_up_prob',
+        y='final_discrep',
+        color='black',
+        ax=ax,
+        label='Final',
+    )
+    ax.legend()
+    ax.set_ylabel('Aggregated λ-discrep')
+    ax.set_xlabel('π(up | junction)')
+    ax.set_title(title)
+    plt.gcf().tight_layout()
+    plt.gcf().subplots_adjust(right=0.75)
 
-    return true_ranges, false_ranges
+#%%
+planning_data = load_analytical_results('results/analytical/tmaze_sweep_junction_pi_old')
+learning_data = load_sampled_results(
+    'results/sample_based/junction-sweep-sampled-3/tmaze_5_two_thirds_up/*')
 
-# %%
-pi_map = {}
-all_sorted_pis = []
-for i, pi in enumerate(sorted(hparam.tmaze_junction_up_pi for hparam in all_results.keys())):
-    pi_map[pi] = i
-    all_sorted_pis.append(pi)
-
-list(all_results.keys())
-
-num_pis = len(all_sorted_pis)
-all_to_plot = {
-    'final_discrep_means': np.zeros(num_pis),
-    'final_discrep_std': np.zeros(num_pis),
-    'initial_discrep_means': np.zeros(num_pis),
-    'initial_discrep_std': np.zeros(num_pis),
-    'is_optimal_means': np.zeros(num_pis),
-    'starting_up_discrep_means': np.zeros((num_pis, 2)),
-    'starting_down_discrep_means': np.zeros((num_pis, 2)),
-    'starting_up_discrep_std': np.zeros((num_pis, 2)),
-    'starting_down_discrep_std': np.zeros((num_pis, 2))
-}
-for hparam, res in all_results.items():
-    all_to_plot['final_discrep_means'][pi_map[
-        hparam.tmaze_junction_up_pi]] = res['final_v_discrep'].mean(axis=-1).mean(axis=0)
-    all_to_plot['final_discrep_std'][pi_map[
-        hparam.tmaze_junction_up_pi]] = res['final_v_discrep'].mean(axis=-1).std(axis=0)
-    all_to_plot['initial_discrep_means'][pi_map[
-        hparam.tmaze_junction_up_pi]] = res['initial_v_discrep'].mean(axis=-1).mean(axis=0)
-    all_to_plot['initial_discrep_std'][pi_map[
-        hparam.tmaze_junction_up_pi]] = res['initial_v_discrep'].mean(axis=-1).std(axis=0)
-    all_to_plot['starting_up_discrep_means'][pi_map[
-        hparam.tmaze_junction_up_pi]] = res['final_v_discrep'][:, :2].mean(axis=0)
-    all_to_plot['starting_up_discrep_std'][pi_map[
-        hparam.tmaze_junction_up_pi]] = res['final_v_discrep'][:, :2].std(axis=0)
-    all_to_plot['is_optimal_means'][pi_map[hparam.tmaze_junction_up_pi]] = res['is_optimal'].mean(axis=0)
-res['final_v_discrep'].shape
-
-# %%
-# Plots for avg across all values.
-fig, ax = plt.subplots(figsize=(5,5))
-
-y_high = 0.3
-y_low = 0
-x_high = all_sorted_pis[-1]
-x_low = all_sorted_pis[0]
-
-final_discrep_means = all_to_plot['final_discrep_means']
-final_discrep_std = all_to_plot['final_discrep_std']
-initial_discrep_means = all_to_plot['initial_discrep_means']
-initial_discrep_std = all_to_plot['initial_discrep_std']
-mean_is_optimal = all_to_plot['is_optimal_means']
-x = all_sorted_pis
-
-# ax.plot(x, initial_discrep_means, color='gray', label='Initial λ-discreps')
-# ax.fill_between(x,
-#                      initial_discrep_means - initial_discrep_std,
-#                      initial_discrep_means + initial_discrep_std,
-#                      color='k',
-#                      alpha=0.2)
-# # ax.set_title('Initial λ-discreps')
-# # ax.set_xlabel('π(up | junction)')
-# ax.set_ylim([y_low, y_high])
-# ax.set_xlim([x_low, x_high])
-
-ax.imshow(mean_is_optimal[None, :],
-               vmin=0,
-               vmax=1,
-               extent=(x_low, x_high, y_low, y_high),
-               cmap='RdYlBu',
-               aspect='auto',
-               alpha=0.5)
-ax.plot(x, initial_discrep_means, color='blue', label='Initial λ-discreps')
-ax.fill_between(x,
-                     initial_discrep_means - initial_discrep_std,
-                     initial_discrep_means + initial_discrep_std,
-                     color='k',
-                     alpha=0.2)
-ax.plot(x, final_discrep_means, color='k', label='Final λ-discreps')
-ax.fill_between(x,
-                     final_discrep_means - final_discrep_std,
-                     final_discrep_means + final_discrep_std,
-                     color='k',
-                     alpha=0.2)
-
-ax.set_xlabel('π(up | junction)')
-ax.set_ylabel('Avg. λ-discreps across obs+')
-ax.set_ylim([y_low, y_high])
-ax.set_xlim([x_low, x_high])
-# fig.text(0.52, 0.0, 'π(up | junction)', ha='center', va='center')
-plt.legend()
-plt.tight_layout()
-# %%
+#%%
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+plot_sweep(planning_data, ax=axes[0], title='Planning Agent', add_colorbar=False)
+plot_sweep(learning_data, ax=axes[1], title='Learning Agent', add_colorbar=True)
