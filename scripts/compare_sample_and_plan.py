e9190814b1869fcef7773f07c50c5e2fbc6d7e41
Ruo Yu Tao <ruoyutao@gmail.com>
2023-02-22 23:34:19 -0500

    fix for MDP running in sample-based, set_policy fix

--- scripts/compare_sample_and_plan.py
+++ scripts/compare_sample_and_plan.py
@@ -1,21 +1,28 @@
+from jax.config import config
+
+config.update('jax_platform_name', 'cpu')
+
 from grl.agents.actorcritic import ActorCritic
 from grl.environment import load_spec
 from grl.mdp import MDP, AbstractMDP
 from grl.utils.policy_eval import analytical_pe
+from grl.memory import memory_cross_product
 
 from learning_agent.memory_iteration import converge_value_functions
 
 if __name__ == "__main__":
     spec_name = "tmaze_eps_hyperparams"
+    # spec_name = "simple_chain"
     corridor_length = 5
     discount = 0.9
     junction_up_pi = 2/3
-    epsilon = 0.1
+    epsilon = 0.0
     error_type = 'l2'
 
     # Sampling agent hyperparams
-    buffer_size = int(4e5)
+    buffer_size = int(4e6)
 
+    print(f"Running sample-based and analytical comparison for Q-values on {spec_name}")
     spec = load_spec(spec_name,
                      memory_id=None,
                      corridor_length=corridor_length,
@@ -27,7 +34,6 @@ if __name__ == "__main__":
     env = AbstractMDP(mdp, spec['phi'])
     pi = spec['Pi_phi'][0]
 
-    analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi, env)
 
     agent = ActorCritic(
         n_obs=env.n_obs,
@@ -39,20 +45,28 @@ if __name__ == "__main__":
         discrep_loss=error_type,
     )
 
-    agent.set_policy(pi)
-    agent.add_memory()
-    agent.reset_memory()
+    agent.set_policy(pi, logits=False)
+
+    # agent.add_memory()
+    # agent.reset_memory()
+    # mem_params = agent.memory_logits
+    # mem_aug_mdp = memory_cross_product(mem_params, env)
+
+    # analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi.repeat(2, axis=0), mem_aug_mdp)
+    analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi, env)
+
     converge_value_functions(agent, env, update_policy=False)
 
     print("Sample-based Q-TD values:")
-    print(agent.q_td)
+    print(agent.q_td.q)
 
     print("Analytical Q-TD values:")
     print(analytical_td_vals['q'])
 
     print("Sample-based Q-MC values:")
-    print(agent.q_mc)
+    print(agent.q_mc.q)
 
     print("Analytical Q-MC values:")
     print(analytical_mc_vals['q'])
 
+    print("done")
