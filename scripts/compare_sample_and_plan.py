2fc7b9a1185ddd2da537257de7ffc17637f75c1b
Ruo Yu Tao <ruoyutao@gmail.com>
2023-02-23 18:17:06 -0500

    set epsilon to 0.2 and add replay buffer saving

--- scripts/compare_sample_and_plan.py
+++ scripts/compare_sample_and_plan.py
@@ -1,5 +1,8 @@
+import numpy as np
 from jax.config import config
+from pathlib import Path
 
+np.set_printoptions(precision=3, suppress=True)
 config.update('jax_platform_name', 'cpu')
 
 from grl.agents.actorcritic import ActorCritic
@@ -8,6 +11,7 @@ from grl.mdp import MDP, AbstractMDP
 from grl.utils.policy_eval import analytical_pe
 from grl.memory import memory_cross_product
 
+from definitions import ROOT_DIR
 from learning_agent.memory_iteration import converge_value_functions
 
 if __name__ == "__main__":
@@ -16,15 +20,16 @@ if __name__ == "__main__":
     corridor_length = 5
     discount = 0.9
     junction_up_pi = 2/3
-    epsilon = 0.0
-    error_type = 'l2'
+    epsilon = 0.2
+    error_type = 'mse'
 
     # Sampling agent hyperparams
     buffer_size = int(4e6)
+    # buffer_size = int(1e3)
 
     print(f"Running sample-based and analytical comparison for Q-values on {spec_name}")
     spec = load_spec(spec_name,
-                     memory_id=None,
+                     memory_id=str(16),
                      corridor_length=corridor_length,
                      discount=discount,
                      junction_up_pi=junction_up_pi,
@@ -47,13 +52,15 @@ if __name__ == "__main__":
 
     agent.set_policy(pi, logits=False)
 
-    # agent.add_memory()
+    agent.add_memory()
     # agent.reset_memory()
     # mem_params = agent.memory_logits
-    # mem_aug_mdp = memory_cross_product(mem_params, env)
+    mem_params = spec['mem_params']
+    agent.set_memory(mem_params, logits=True)
+    mem_aug_mdp = memory_cross_product(mem_params, env)
 
-    # analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi.repeat(2, axis=0), mem_aug_mdp)
-    analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi, env)
+    analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi.repeat(2, axis=0), mem_aug_mdp)
+    # analytical_state_vals, analytical_mc_vals, analytical_td_vals, info = analytical_pe(pi, env)
 
     converge_value_functions(agent, env, update_policy=False)
 
@@ -70,3 +77,10 @@ if __name__ == "__main__":
     print(analytical_mc_vals['q'])
 
     print("done")
+
+    buffer_dir = Path(ROOT_DIR, 'scripts', 'results', 'sample_based')
+
+    fname = f'replaymemory_corridor({corridor_length})_eps({epsilon})_size({buffer_size})'
+    ext = '.pkl'
+    print(f"Saving to {buffer_dir / (fname + ext)}")
+    agent.replay.save(buffer_dir, filename=fname, extension=ext)
