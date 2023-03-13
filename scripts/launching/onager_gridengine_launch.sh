5067afc446b3495b5b40b61e379b352acb0873c4
Ruo Yu Tao <ruoyutao@gmail.com>
2023-03-12 21:48:49 -0400

    gridengine leaky exp launch file

--- scripts/launching/onager_gridengine_launch.sh
+++ scripts/launching/onager_gridengine_launch.sh
@@ -2,9 +2,10 @@ cd ../../
 
 onager launch \
     --backend gridengine \
-    --jobname tmaze_sweep_eps_uniform \
-    --mem 1 \
-    --cpus 2 \
+    --jobname tmaze_sweep_eps_leaky \
+    --tasks-per-node 4 \
+    --mem 3 \
+    --cpus 5 \
     --duration 0-03:00:00 \
     --venv venv \
     -q '*@@mblade12'\
