433d29fcb70e70a597d23a634cfb30b738b508d6
Ruo Yu Tao <ruoyutao@gmail.com>
2023-03-12 21:26:27 -0400

    update onager slurm again, for 4 jobs per node

--- scripts/launching/onager_slurm_launch.sh
+++ scripts/launching/onager_slurm_launch.sh
@@ -3,8 +3,8 @@ cd ../../
 onager launch \
     --backend slurm \
     --jobname tmaze_sweep_junction_pi_leaky \
-    --tasks-per-node 2 \
-    --mem 2 \
-    --cpus 4 \
+    --tasks-per-node 4 \
+    --mem 3 \
+    --cpus 5 \
     --duration 0-01:00:00 \
     --venv venv \
