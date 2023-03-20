be46171ddbc1c581260d9d00788b6ecabc3e17c0
Cam Allen <csal@brown.edu>
2023-03-20 08:33:17 -0400

    Update samples vs lambda plots

--- scripts/plotting/samples_vs_lambda_vs_value_error.py
+++ scripts/plotting/samples_vs_lambda_vs_value_error.py
@@ -28,6 +28,8 @@ for results_dir in sorted(glob.glob('results/sample_based/compare_sample_and_pla
             'lambda': lambda0,
             'value_error': value_error_0,
             'learner': 'active',
+            'lstd_q': lstd_q0,
+            'samp_q': samp_q0,
         })
 
         lambda1 = info['args']['lambda1']
@@ -40,11 +42,56 @@ for results_dir in sorted(glob.glob('results/sample_based/compare_sample_and_pla
             'lambda': lambda1,
             'value_error': value_error_1,
             'learner': 'passive',
+            'lstd_q': lstd_q1,
+            'samp_q': samp_q1,
         })
 
 df = pd.DataFrame(data)
 
 #%%
-sns.lineplot(data=df.query('n_samples>=1e6'), x='lambda', y='value_error', hue='n_samples', palette='colorblind')
+mc = df[np.isclose(df["lambda"], 0.95)]
+td = df[np.isclose(df["lambda"], 0)]
+lstd_discrep = (np.array(mc.lstd_q.values[0] - td.lstd_q.values[0])**2).mean()
+
+samp_discreps = []
+for mc_row, td_row, mc_nsamp, td_nsamp in zip(mc.samp_q.values, td.samp_q.values, mc.n_samples.values, td.n_samples.values):
+    assert mc_nsamp == td_nsamp
+    discrep = (np.array(mc_row - td_row)**2).mean()
+    samp_discreps.append({
+        'n_samples': mc_nsamp,
+        'samp_discrep': discrep,
+    })
+samp_discreps = pd.DataFrame(samp_discreps)
+
+
+#%%
+sns.lineplot(data=df, x='lambda', y='value_error', hue='n_samples', style='n_samples', palette='colorblind')
 plt.ylabel('Q-function MSE')
 plt.xlabel('Lambda')
+
+#%%
+sns.lineplot(data=df.query('n_samples==1e6'), x='lambda', y='value_error', hue='n_samples', palette='colorblind')
+plt.ylabel('Q-function MSE')
+plt.xlabel('Lambda')
+
+#%%
+sns.lineplot(data=df.query('n_samples==1e7'), x='lambda', y='value_error', hue='n_samples', palette='colorblind')
+plt.ylabel('Q-function MSE')
+plt.xlabel('Lambda')
+
+#%%
+sns.lineplot(data=df, x='n_samples', y='value_error', hue='lambda')
+plt.hlines(lstd_discrep, df.n_samples.min(), df.n_samples.max(), linestyle='--', color='r')
+plt.text(x=df.n_samples.min(), y=lstd_discrep+0.03, s=r'$D_\lambda$ for $\lambda \in \{0, 1\}$ (analytical)', ha='left')
+plt.semilogx()
+plt.ylabel('Q-function MSE')
+plt.xlabel('Number of samples')
+
+#%%
+sns.lineplot(data=samp_discreps, x='n_samples', y='samp_discrep')
+plt.semilogx()
+plt.hlines(lstd_discrep, df.n_samples.min(), df.n_samples.max(), linestyle='--', color='r')
+plt.text(x=df.n_samples.median(), y=lstd_discrep-0.005, s=r'$D_\lambda$ for $\lambda \in \{0, 1\}$ (analytical)', ha='center')
+plt.semilogx()
+plt.ylabel(r'Estimated $\lambda$ discrepancy')
+plt.xlabel('Number of samples')
