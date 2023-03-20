687eff925587945b700675ee8d4dd628c8e21589
Ruo Yu Tao <ruoyutao@gmail.com>
2023-03-20 16:39:43 -0400

    add hyperparams for sweeping lambda1

---
+++ scripts/hyperparams/tmaze_sweep_eps_lambda1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+hparams = {
+    'file_name':
+        'runs_tmaze_sweep_eps_lambda1.txt',
+    'args': [{
+        'spec': 'tmaze_eps_hyperparams',
+        'tmaze_corridor_length': 5,
+        'tmaze_discount': 0.9,
+        'tmaze_junction_up_pi': 1.,
+        'epsilon': np.linspace(0, 1, num=13),
+        'use_memory': '0',
+        'value_type': 'q',
+        'lambda_1': [0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 0.999, 0.9999],
+        'alpha': 1.,
+        'mi_steps': 50000,
+        'pi_steps': 0,
+        'init_pi': 0,
+        'error_type': 'l2',
+        'lr': 1,
+        'seed': [2020 + i for i in range(10)],
+    }]
+}
--- scripts/launching/onager_slurm_launch.sh
+++ scripts/launching/onager_slurm_launch.sh
@@ -2,9 +2,9 @@ cd ../../
 
 onager launch \
     --backend slurm \
-    --jobname tmaze_sweep_eps_leaky \
+    --jobname tmaze_sweep_eps_lambda1 \
     --tasks-per-node 4 \
     --mem 3 \
     --cpus 5 \
-    --duration 0-01:00:00 \
+    --duration 0-03:00:00 \
     --venv venv \
