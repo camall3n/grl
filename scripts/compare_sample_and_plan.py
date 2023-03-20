2b6bc1983ab532486590bef6e6d13f5f09c6e7f2
Cam Allen <csal@brown.edu>
2023-03-20 08:35:10 -0400

    Increase lambda sweep spacing to 0.1

--- scripts/compare_sample_and_plan.py
+++ scripts/compare_sample_and_plan.py
@@ -17,7 +17,7 @@ config.update('jax_platform_name', 'cpu')
 
 args = parse_args()
 args.min_mem_opt_replay_size = args.replay_buffer_size
-args.lambda1 = args.lambda0 + 0.05
+args.lambda1 = args.lambda0 + 0.1
 del args.f
 
 spec_name = "tmaze_eps_hyperparams"
