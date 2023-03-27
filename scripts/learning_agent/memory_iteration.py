a169bdb67a2154a178ba8183c4fdc1fb1e4e644d
Cam Allen <csal@brown.edu>
2023-03-27 18:10:25 -0400

    Use cast_as_int for large arguments

--- scripts/learning_agent/memory_iteration.py
+++ scripts/learning_agent/memory_iteration.py
@@ -32,8 +32,8 @@ def parse_args():
     parser.add_argument('--n_memory_trials', type=int, default=500)
     parser.add_argument('--n_memory_iterations', type=int, default=2)
     parser.add_argument('--n_policy_iterations', type=int, default=10)
-    parser.add_argument('--n_samples_per_policy', type=int, default=1e7)
-    parser.add_argument('--min_mem_opt_replay_size', type=int, default=1e7,
+    parser.add_argument('--n_samples_per_policy', type=cast_as_int, default=1e7)
+    parser.add_argument('--min_mem_opt_replay_size', type=cast_as_int, default=1e7,
         help="Minimum number of experiences in replay buffer for memory optimization")
     # parser.add_argument('--use_min_replay_num_samples', action='store_true')
     parser.add_argument('--replay_buffer_size', type=cast_as_int, default=2e7)
