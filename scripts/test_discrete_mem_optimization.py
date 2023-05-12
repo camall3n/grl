ecd3439d601b189f2860c0ef6f4c504560a1779a
Aaron Kirtland <atkirtland@gmail.com>
2023-05-12 18:38:23 -0400

    Added simulated annealing

--- scripts/test_discrete_mem_optimization.py
+++ scripts/test_discrete_mem_optimization.py
@@ -16,6 +16,9 @@ from grl.utils.loss import discrep_loss
 from grl.memory import memory_cross_product
 from scripts.learning_agent.memory_iteration import parse_args
 
+import math
+import matplotlib.pyplot as plt
+
 np.set_printoptions(precision=3, suppress=True)
 config.update('jax_platform_name', 'cpu')
 
@@ -96,6 +99,13 @@ class SearchNode:
         new_probs[action, obs] = mem_op
         return SearchNode(new_probs)
 
+    def modify_rowcol(self, action, obs, mem_row, mem_col):
+        new_probs = self.mem_probs.copy()
+        new_row = np.zeros((1,learning_agent.n_mem_states))
+        new_row[0,mem_col] = 1
+        new_probs[action, obs][mem_row] = new_row
+        return SearchNode(new_probs)
+
     def get_successors(self, skip_hashes=set()):
         skip_hashes.add(self.mem_hash)
         n_actions, n_obs, _, _ = self.mem_probs.shape
@@ -107,13 +117,30 @@ class SearchNode:
                     if successor.mem_hash not in skip_hashes:
                         successors.append(successor)
         return successors
+    
+    def get_random_successor(self, modify_row=True):
+        n_actions, n_obs, _, _ = self.mem_probs.shape
+        a = np.random.choice(n_actions)
+        o = np.random.choice(n_actions)
+
+        if modify_row: 
+            mem_row = np.random.choice(learning_agent.n_mem_states)
+            mem_col = np.random.choice(learning_agent.n_mem_states)
+            successor = self.modify_rowcol(a, o, mem_row, mem_col)
+        else:
+            # for the original modify
+            mem_ops = [HOLD, TOGGLE, SET, RESET]
+            mem_op_idx = np.random.choice(range(len(mem_ops)))
+            mem_op = mem_ops[mem_op_idx]
+        
+        return successor
 
     def evaluate(self):
         learning_agent.set_memory(self.mem_probs, logits=False)
         mem_aug_mdp = memory_cross_product(learning_agent.memory_logits, env)
         return discrep_loss(learning_agent.policy_probs, mem_aug_mdp)
 
-def optimize_memory(mem_probs):
+def queue_search(mem_probs):
     s = SearchNode(mem_probs)
 
     visited = set()
@@ -126,11 +153,11 @@ def optimize_memory(mem_probs):
         node = frontier.popleft()
         discrep = node.evaluate()[0] + np.random.normal(loc=0, scale=0.00)
         n_evals += 1
-        print(f'discrep = {discrep}')
+        # print(f'discrep = {discrep}')
         visited.add(node.mem_hash)
 
         if discrep < best_discrep:
-            print(f'New best discrep: {discrep}')
+            # print(f'New best discrep: {discrep}')
             best_discrep = discrep
             best_node = node
             successors = node.get_successors(skip_hashes=visited)
@@ -142,18 +169,68 @@ def optimize_memory(mem_probs):
     }
     return best_node, info
 
-best_node, info = optimize_memory(mem_probs)
+def simulated_annealing(mem_probs, beta=1e3, cooling_rate=0.99, n=200):
+    # simulated annealing
+    # beta = 1/temp
+    discs = []
+    s = SearchNode(mem_probs)
+    node = s
+    p = node.evaluate()[0]
+    best_node = s
+    best_p = p
+    for i in range(n):
+        successor = node.get_random_successor()
+        p2 = successor.evaluate()[0]
+        de = p2 - p
+        if p2 == p:
+            accept = 0
+        else:
+            accept = math.exp(-de*beta)
+        # decide whether to accept the transition
+        if de <= 0:
+            node = successor
+            p = p2
+            if p < best_p:
+                best_p = p
+                best_node = node
+        elif np.random.random() < accept:
+            node = successor
+            p = p2
+        if i % 1 == 0:
+            discs.append(p)
+        beta /= cooling_rate
+    info = {
+        'best_discrep': discs[-1].tolist(),
+        'beta': beta,
+        'cooling_rate': cooling_rate,
+        'n': n,
+    }
+    return best_node, info, discs
+    
+mode = "queue"
+assert mode in ["queue", "sa"], "invalid mode"
+
+if mode == "queue":
+    best_node, info = queue_search(mem_probs)
+    M = learning_agent.n_mem_states
+    O = learning_agent.n_obs
+    A = learning_agent.n_actions
+    n_mem_fns = M**(M * O * A)
+    print(f'Total memory functions: {n_mem_fns}')
+    print(f'Number evaluated: {info["n_evals"]}')
+    
+elif mode == "sa":
+    beta = 1e3
+    cooling_rate = 0.99
+    n = 200
+    best_node, info, discs = simulated_annealing(mem_probs, beta, cooling_rate, n)
+    plt.plot(range(len(discs)), discs)
+    plt.show()
 
 # Search results stuff
 print()
 print(f'Best node: \n'
       f'{best_node.mem_probs}\n')
-M = learning_agent.n_mem_states
-O = learning_agent.n_obs
-A = learning_agent.n_actions
-n_mem_fns = M**(M * O * A)
-print(f'Total memory functions: {n_mem_fns}')
-print(f'Number evaluated: {info["n_evals"]}')
 print(f'Best discrep: {info["best_discrep"]}')
 
 # Final performance stuff
