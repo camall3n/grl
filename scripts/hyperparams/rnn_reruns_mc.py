28982a00086aa11c85c23fd5380ad33f63a26f23
Ruo Yu Tao <ruoyutao@gmail.com>
2023-06-10 22:27:34 +0800

    ok now run lambda_coeff = 0 only

--- scripts/hyperparams/rnn_reruns_mc.py
+++ scripts/hyperparams/rnn_reruns_mc.py
@@ -38,10 +38,10 @@ hparams = {
 
             # Multihead RNN/Lambda discrep
             'multihead_action_mode': 'mc',
-            # 'multihead_loss_mode': ['both', 'mc'],
-            'multihead_loss_mode': ['both'],
+            'multihead_loss_mode': ['both', 'mc'],
+            # 'multihead_loss_mode': ['both'],
             # 'multihead_lambda_coeff': [-1, 0., 1.],
-            'multihead_lambda_coeff': [-1, 1.],
+            'multihead_lambda_coeff': 0.,
             'normalize_rewards': True,
 
             # Replay
