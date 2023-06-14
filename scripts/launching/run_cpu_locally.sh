3e3dcd36d531574d7a796ab64b386902e3d58e0f
Ruo Yu Tao <ruoyutao@gmail.com>
2023-06-14 14:42:15 +0800

    add sed'ed run_cpu_locally to specify lines to run for run file

--- scripts/launching/run_cpu_locally.sh
+++ scripts/launching/run_cpu_locally.sh
@@ -3,4 +3,8 @@ export CUDA_VISIBLE_DEVICES=0
 
 cd ../../
 source venv/bin/activate
-parallel --eta -u < 'scripts/runs/runs_rnn_split.txt'
+
+TO_RUN=$(sed -n "101,800p" scripts/runs/runs_rnn_reruns_sweep_td.txt)
+
+#parallel --eta -u < 'scripts/runs/runs_rnn_split.txt'
+parallel --eta -u ::: "$TO_RUN"
