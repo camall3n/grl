f7e06a65cfa1b70f8cab31dd805f6a13ce25045f
Ruo Yu Tao <ruoyutao@gmail.com>
2023-06-14 14:56:04 +0800

    go back to just reading a file

--- scripts/launching/run_cpu_locally.sh
+++ scripts/launching/run_cpu_locally.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0
 cd ../../
 source venv/bin/activate
 
-TO_RUN=$(sed -n "101,800p" scripts/runs/runs_rnn_reruns_sweep_td.txt)
+#TO_RUN=$(sed -n "101,800p" scripts/runs/runs_rnn_reruns_sweep_td.txt)
 
-#parallel --eta -u < 'scripts/runs/runs_rnn_split.txt'
-parallel --eta -u < "$TO_RUN"
+parallel --eta -u < 'scripts/runs/runs_rnn_reruns_sweep_td.txt'
+#parallel --eta -u < "$TO_RUN"
