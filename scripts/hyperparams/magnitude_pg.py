4eec97fb41ea1334fe5f1771bb49666ad7ff063b
Ruo Yu Tao <ruoyutao@gmail.com>
2023-12-04 11:04:39 -0500

    add magnitude pg runs

---
+++ scripts/hyperparams/magnitude_pg.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+exp_name = Path(__file__).stem
+
+hparams = {
+    'file_name':
+        f'runs_{exp_name}.txt',
+    'args': [{
+        'spec': [
+            'tiger-alt-start', 'tmaze_5_two_thirds_up', 'example_7', '4x3.95',
+            'cheese.95', 'network', 'shuttle.95', 'paint.95'
+            # 'hallway'
+            # 'bridge-repair',
+        ],
+        'policy_optim_alg': 'policy_grad',
+        'value_type': 'q',
+        'error_type': 'l2',
+        'alpha': 1.,
+        # 'objective': 'obs_space',
+        'objective': 'magnitude',
+        'mi_steps': 20000,
+        'pi_steps': 10000,
+        'optimizer': 'adam',
+        'lr': 0.01,
+        'use_memory': 0,
+        'n_mem_states': [2, 4, 8],
+        'mi_iterations': 1,
+        'seed': [2020 + i for i in range(30)],
+    }]
+}
