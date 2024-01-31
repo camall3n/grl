e66371aca557bf1fd5f3a267b934a874cfc883fe
Ruo Yu Tao <ruoyutao@gmail.com>
2024-01-31 18:53:29 -0500

    split 8 mem state runs into two seperate runs

--- scripts/hyperparams/batch_run_pg.py
+++ scripts/hyperparams/batch_run_pg.py
@@ -22,10 +22,34 @@ hparams = {
         'pi_steps': 10000,
         'optimizer': 'adam',
         'lr': 0.01,
-        'n_mem_states': [2, 4, 8],
+        'n_mem_states': [2, 4],
         'mi_iterations': 1,
         'random_policies': 10,
         'n_seeds': 10,
         'platform': 'gpu'
-    }]
+    }, {
+        'spec': ['paint.95', 'tmaze_5_two_thirds_up'],
+        # 'spec': [
+        #     'tiger-alt-start', 'tmaze_5_two_thirds_up', 'example_7', '4x3.95',
+        #     'cheese.95', 'network', 'shuttle.95', 'paint.95'
+        #     # 'hallway'
+        #     # 'bridge-repair',
+        # ],
+        'policy_optim_alg': 'policy_grad',
+        'value_type': 'q',
+        'error_type': 'l2',
+        'alpha': 1.,
+        'mi_steps': 20000,
+        'pi_steps': 10000,
+        'optimizer': 'adam',
+        'lr': 0.01,
+        'n_mem_states': 8,
+        'mi_iterations': 1,
+        'random_policies': 10,
+        'n_seeds': 5,
+        'seed': [2024, 2025]
+        'platform': 'gpu'
+    }
+
+    ]
 }
