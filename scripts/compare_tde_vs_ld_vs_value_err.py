039c2d93ea83b8c26fade34c3c855739af893823
Cam Allen <csal@brown.edu>
2024-02-12 10:52:26 -0800

    Update compare script

--- scripts/compare_tde_vs_ld_vs_value_err.py
+++ scripts/compare_tde_vs_ld_vs_value_err.py
@@ -6,15 +6,18 @@ from time import time
 import numpy as np
 import jax
 from jax.config import config
+from jax.nn import softmax
 
 from grl.environment import load_pomdp
 from grl.environment.policy_lib import get_start_pi
-from grl.utils.loss import mstd_err, discrep_loss
+from grl.utils.loss import mstd_err, discrep_loss, value_error
 from grl.utils.file_system import results_path, numpyify_and_save
-from grl.memory import get_memory
+from grl.memory import get_memory, memory_cross_product
 from grl.memory_iteration import run_memory_iteration
 from grl.utils.mdp import functional_get_occupancy
+from grl.utils.augment_policy import construct_aug_policy
 from grl.vi import td_pe
+
 
 spec = 'example_20'
 seed = 42
@@ -57,7 +60,33 @@ print('r_bad - q_dn:', (-1) - q_dn)
 print('r_good - q_dn:', 1 - q_dn)
 print('c_s:', c_s)
 
-mstd_err(pi_phi, pomdp, value_type='q')
-discrep_loss(pi_phi, pomdp, value_type='q')
-c_total = float(sum(c_s[:3]))
-0.5*0.061765919396285174**2 + 0.5*( (c_s[0]+c_s[1])*(-0.24508320730324495)**2 + c_s[2]*(1.754916792696755**2) ) / c_total
+mstd_err(pi_phi, pomdp, value_type='q')[0]
+discrep_loss(pi_phi, pomdp, value_type='q')[0]
+value_error(pi_phi, pomdp, value_type='q')[0]
+
+mem_hold = get_memory('101')
+mem_hold.shape
+pomdp_hold = memory_cross_product(mem_hold, pomdp)
+
+mem_flip = get_memory('102')
+mem_flip.shape
+pomdp_flip = memory_cross_product(mem_flip, pomdp)
+
+mem_set = get_memory('103')
+pomdp_set = memory_cross_product(mem_set, pomdp)
+
+n_mem_states = 2
+pi_augmented = (
+    np.expand_dims(pi_phi, 1)
+    .repeat(n_mem_states, 1)
+    .reshape(pomdp.observation_space.n * n_mem_states, pomdp.action_space.n)
+)
+
+mstd_err(pi_augmented, pomdp_flip, value_type='q')[0]
+discrep_loss(pi_augmented, pomdp_flip, value_type='q')[0]
+value_error(pi_augmented, pomdp_flip, value_type='q')[0]
+
+
+mstd_err(pi_augmented, pomdp_set, value_type='q')[0]
+discrep_loss(pi_augmented, pomdp_set, value_type='q')[0]
+value_error(pi_augmented, pomdp_set, value_type='q')[0]
