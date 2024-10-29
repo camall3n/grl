264d20354147ea0c007c35b6eaebd4119dc29abb
Cam Allen <camallen@berkeley.edu>
2024-10-28 22:54:35 -0700

    Plot both discounted/undiscounted rnn results

--- scripts/knife_edge/rnn_results.py
+++ scripts/knife_edge/rnn_results.py
@@ -12,11 +12,11 @@ mpl.rcParams.update({
     "font.serif": ["Computer Modern Roman"],
     "font.sans-serif": ["Computer Modern Sans serif"],
     "font.monospace": ["Computer Modern Typewriter"],
-    "axes.labelsize": 12,  # LaTeX default is 10pt
-    "font.size": 12,
+    "axes.labelsize": 14,  # LaTeX default is 10pt
+    "font.size": 14,
     "legend.fontsize": 12,
-    "xtick.labelsize": 12,
-    "ytick.labelsize": 12,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
 })
 
 hex = [
@@ -107,5 +107,61 @@ axes[-1].legend(handles=handles, labels=texts, loc='lower right', fancybox=True,
 plt.tight_layout()
 if not test:
     plt.savefig(f'undiscounted-rnn{"-1x4x3" if col_wrap is None else "-2x2x3"}.png', dpi=300)
+# plt.savefig('nice-rnn.png')
+plt.show()
+
+
+#%%
+
+data = pd.read_csv('results_rnn_2024_07_09.csv')
+data['seed'].unique()[0]
+g = sns.relplot(
+    data=data.query('seed==0') if test else data,
+    x='timestep',
+    y='score',
+    hue='algo',
+    col='env',
+    col_wrap=col_wrap,
+    kind='line',
+    facet_kws={'sharey': False, 'sharex': False},
+    palette=palette,
+    height=2.5 if col_wrap==2 else 3,
+    aspect=1.4 if col_wrap==2 else 1,
+)
+
+# Clean up the titles
+axes = g.axes.flatten()
+titles = [
+    r'RockSample $11\times 11$',
+    r'RockSample $15\times 15$',
+    r'Battleship $10\times 10$',
+    r'Partially Observable PacMan'
+]
+for ax, title in zip(axes, titles):
+    ax.set_title(title)
+    ax.set_xlabel('Timestep')
+    ax.set_ylabel('')
+
+axes[0].set_ylabel('Discounted Return')
+axes[2].set_ylabel('Discounted Return')
+
+# Clean up the legend
+leg = g._legend
+leg.set_bbox_to_anchor([0.1,0.8])
+handles = leg.legend_handles
+texts = [x.get_text() for x in leg.texts]
+nice_algo_names = {
+    'ld_ppo': r'PPO+RNN+LD',
+    'ppo': r'PPO+RNN',
+    'memoryless_ppo': 'PPO',
+}
+texts = [nice_algo_names[text] for text in texts]
+title = leg.get_title().get_text()
+leg.remove()
+axes[-1].legend(handles=handles, labels=texts, loc='lower right', fancybox=True, framealpha=0.7)
+
+plt.tight_layout()
+if not test:
+    plt.savefig(f'nice-rnn{"" if col_wrap is None else "-2x2"}.png', dpi=300)
 # plt.savefig('nice-rnn.png')
 plt.show()
